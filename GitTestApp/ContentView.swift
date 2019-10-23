2ac5df86537b7a45485fa0a3930379c165b980ba
shobharajput0710 <shobharajput0710@gmail.com>
2019-10-23 18:24:34 +0530

    Initial Commit

---
+++ GitTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitTestApp
+//
+//  Created by Shobha Kumari on 01/08/1941 Saka.
+//  Copyright © 1941 Shobha Kumari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
